c3234a7113f083ee01f5af744726c608dd3a2593
chenjiayin <chenjiayin@thunisoft.com>
2021-02-05 17:59:22 +0800

    更新示例

--- picocli-test/src/main/java/com/blinkfox/test/HelloWorld.java
+++ picocli-test/src/main/java/com/blinkfox/test/HelloWorld.java
@@ -1,16 +1,18 @@
 package com.blinkfox.test;
 
-import io.quarkus.picocli.runtime.annotations.TopCommand;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import javax.inject.Inject;
 import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
 
 /**
- * top command.
+ * HelloWorld.
  *
  * @author blinkfox on 2021-02-05.
- * @since 1.0.0
+ * @since v1.0.0
  */
-@TopCommand
+@QuarkusMain
 @CommandLine.Command(
         version = "1.0.0",
         description = "@|bold,green Hello World!|@",
@@ -19,7 +21,10 @@ import picocli.CommandLine;
         parameterListHeading = "%n@|bold,blue,underline Parameters|@:%n",
         optionListHeading = "%n@|bold,blue,underline Options|@:%n",
         commandListHeading = "%n@|bold,blue,underline Commands|@:%n")
-public class EntryCommand implements Runnable {
+public class HelloWorld implements Runnable, QuarkusApplication {
+
+    @Inject
+    CommandLine.IFactory factory;
 
     /**
      * 是否可查看帮助信息.
@@ -34,16 +39,20 @@ public class EntryCommand implements Runnable {
 
     @Override
     public void run() {
-        AnsiConsole.systemInstall();
-        System.out.println();
         System.out.println(CommandLine.Help.Ansi.AUTO
                 .string("@|blue ------------------------|@"));
         System.out.println(CommandLine.Help.Ansi.AUTO
                 .string("@|bold,magenta Hello World!|@"));
         System.out.println(CommandLine.Help.Ansi.AUTO
                 .string("@|blue ------------------------|@"));
-        System.out.println();
+    }
+
+    @Override
+    public int run(String... args) {
+        AnsiConsole.systemInstall();
+        int code = new CommandLine(this, factory).execute(args);
         AnsiConsole.systemUninstall();
+        return code;
     }
 
 }
